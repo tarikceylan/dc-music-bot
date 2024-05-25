54ecac0c7d8315fda85c32d935bea7f325cec86e
tarikceylan <tarikceylan@outlook.com>
2024-05-25 19:20:39 +0300

    v1.1.1

--- src/app.ts
+++ src/app.ts
@@ -3,15 +3,12 @@ import 'dotenv/config';
 import { executeCommand } from './core/execute';
 import { bindCommands } from './utils';
 import { voxClient } from './core';
-import { Client, Events } from 'discord.js';
-import { deployGuildCommands } from './utils/deployGuildCommands';
 import { registerClientEvents, registerGuildEvents } from './events';
 
 registerClientEvents(voxClient);
 registerGuildEvents(voxClient);
 
 bindCommands(voxClient);
-
 executeCommand(voxClient);
 
 voxClient.login(process.env.TOKEN);
